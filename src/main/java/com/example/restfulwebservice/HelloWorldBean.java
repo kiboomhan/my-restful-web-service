e0adbc951b469ff187dff45539bffa0d9ad3cd04
kiboomhan <kiboomhan@gmail.com>
2021-08-17 16:38:02 +0900

    Completing Section 1

---
+++ src/main/java/com/example/restfulwebservice/HelloWorldBean.java
@@ -0,0 +1,14 @@
+package com.example.restfulwebservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor // default 생성자
+public class HelloWorldBean {
+    private String message;
+
+
+}
