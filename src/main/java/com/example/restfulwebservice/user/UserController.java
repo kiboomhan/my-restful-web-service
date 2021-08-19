bfba812d44e1fa4716d4a84c6891de683396f3bd
kiboomhan <kiboomhan@gmail.com>
2021-08-19 14:59:16 +0900

    - Level3 HATEOAS 적용 - Swagger 적용 - Actuator 적용 - HAL Browser 적용 - Spring Security

--- src/main/java/com/example/restfulwebservice/user/UserController.java
+++ src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -1,6 +1,9 @@
 package com.example.restfulwebservice.user;
 
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.EntityLinks;
+import org.springframework.hateoas.server.ExposesResourceFor;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,12 +17,16 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
+@ExposesResourceFor(User.class)
 public class UserController {
+
+    private final EntityLinks entityLinks;
 
     // @Autowired
     private UserDaoService userDaoService;
 
-    public UserController(UserDaoService userDaoService) {
+    public UserController(EntityLinks entityLinks, UserDaoService userDaoService) {
+        this.entityLinks = entityLinks;
         this.userDaoService = userDaoService;
     }
 
@@ -39,12 +46,30 @@ public class UserController {
         // HATEOASES
         EntityModel<User> model = EntityModel.of(user);
 
+        Link link = entityLinks.linkToItemResource(User.class, id);
         WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
         model.add(linkTo.withRel("all-users"));
+        model.add(link);
 
         return ResponseEntity.ok(model);
     }
 
+    @GetMapping("/userstest/{id}")
+    public EntityModel<User> retrieveUserTest(@PathVariable int id) {
+        User user = userDaoService.findOne(id);
+
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        // HATEOASES
+        EntityModel<User> model = new EntityModel<>(user); // EntityModel.of(user);
+
+        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        model.add(linkTo.withRel("all-users"));
+
+        return model;
+    }
 
     @PostMapping("/users")
     public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
